e4e2eefb2d08c12000ef58523fbb0e5272937964
Eric Hester <eric.hester@umbrella.tech>
2025-02-19 08:35:09 -0500

    removed specififc template references

--- netbox_asset_management/views.py
+++ netbox_asset_management/views.py
@@ -61,33 +61,27 @@ class InventoryItemAssetView(BaseAssetView):
 class PurchaseOrderListView(ObjectListView):
     queryset = PurchaseOrder.objects.all()
     table = PurchaseOrderTable
-    template_name = 'netbox_asset_management/purchaseorder_list.html'
     filterset = PurchaseOrderFilterSet
 
 class PurchaseOrderDetailView(ObjectView):
     queryset = PurchaseOrder.objects.all()
-    template_name = 'netbox_asset_management/purchaseorder_detail.html'
 
 class PurchaseOrderCreateView(ObjectEditView):
     model = PurchaseOrder
     form_class = PurchaseOrderForm
-    template_name = 'netbox_asset_management/purchaseorder_edit.html'
 
 class PurchaseOrderUpdateView(ObjectEditView):
     model = PurchaseOrder
     form_class = PurchaseOrderForm
-    template_name = 'netbox_asset_management/purchaseorder_edit.html'
 
 class PurchaseOrderDeleteView(ObjectDeleteView):
     model = PurchaseOrder
-    template_name = 'netbox_asset_management/purchaseorder_delete.html'
 
 class PurchaseOrderBulkImportView(BulkImportView):
     queryset = PurchaseOrder.objects.all()
     model = PurchaseOrder
     table = PurchaseOrderTable
     form = PurchaseOrderBulkImportForm
-    template_name = 'netbox_asset_management/purchaseorder_import.html'
     default_return_url = 'plugins:netbox_asset_management:purchaseorder_list'
 
 class PurchaseOrderBulkEditView(BulkEditView):
@@ -95,7 +89,6 @@ class PurchaseOrderBulkEditView(BulkEditView):
     filterset = PurchaseOrderFilterSet
     table = PurchaseOrderTable
     form = PurchaseOrderForm
-    template_name = 'netbox_asset_management/purchaseorder_bulk_edit.html'
     default_return_url = 'plugins:netbox_asset_management:purchaseorder_list'
 
 class PurchaseOrderBulkDeleteView(BulkDeleteView):
@@ -112,33 +105,27 @@ class PurchaseOrderChangelogView(ObjectChangeLogView):
 class LicenseListView(ObjectListView):
     queryset = License.objects.all()
     table = LicenseTable
-    template_name = 'netbox_asset_management/license_list.html'
     filterset = LicenseFilterSet
 
 class LicenseDetailView(ObjectView):
     queryset = License.objects.all()
-    template_name = 'netbox_asset_management/license_detail.html'
 
 class LicenseCreateView(ObjectEditView):
     model = License
     form_class = LicenseForm
-    template_name = 'netbox_asset_management/license_edit.html'
 
 class LicenseUpdateView(ObjectEditView):
     model = License
     form_class = LicenseForm
-    template_name = 'netbox_asset_management/license_edit.html'
 
 class LicenseDeleteView(ObjectDeleteView):
     model = License
-    template_name = 'netbox_asset_management/license_delete.html'
 
 class LicenseBulkImportView(BulkImportView):
     queryset = License.objects.all()
     model = License
     table = LicenseTable
     form = LicenseBulkImportForm
-    template_name = 'netbox_asset_management/license_import.html'
     default_return_url = 'plugins:netbox_asset_management:license_list'
 
 class LicenseBulkEditView(BulkEditView):
@@ -146,7 +133,6 @@ class LicenseBulkEditView(BulkEditView):
     filterset = LicenseFilterSet
     table = LicenseTable
     form = LicenseForm
-    template_name = 'netbox_asset_management/license_bulk_edit.html'
     default_return_url = 'plugins:netbox_asset_management:license_list'
 
 class LicenseBulkDeleteView(BulkDeleteView):
@@ -163,33 +149,27 @@ class LicenseChangelogView(ObjectChangeLogView):
 class SupportContractListView(ObjectListView):
     queryset = SupportContract.objects.all()
     table = SupportContractTable
-    template_name = 'netbox_asset_management/supportcontract_list.html'
     filterset = SupportContractFilterSet
 
 class SupportContractDetailView(ObjectView):
     queryset = SupportContract.objects.all()
-    template_name = 'netbox_asset_management/supportcontract_detail.html'
 
 class SupportContractCreateView(ObjectEditView):
     model = SupportContract
     form_class = SupportContractForm
-    template_name = 'netbox_asset_management/supportcontract_edit.html'
 
 class SupportContractUpdateView(ObjectEditView):
     model = SupportContract
     form_class = SupportContractForm
-    template_name = 'netbox_asset_management/supportcontract_edit.html'
 
 class SupportContractDeleteView(ObjectDeleteView):
     model = SupportContract
-    template_name = 'netbox_asset_management/supportcontract_delete.html'
 
 class SupportContractBulkImportView(BulkImportView):
     queryset = SupportContract.objects.all()
     model = SupportContract
     table = SupportContractTable
     form = SupportContractBulkImportForm
-    template_name = 'netbox_asset_management/supportcontract_import.html'
     default_return_url = 'plugins:netbox_asset_management:supportcontract_list'
 
 class SupportContractBulkEditView(BulkEditView):
@@ -197,7 +177,6 @@ class SupportContractBulkEditView(BulkEditView):
     filterset = SupportContractFilterSet
     table = SupportContractTable
     form = SupportContractForm
-    template_name = 'netbox_asset_management/supportcontract_bulk_edit.html'
     default_return_url = 'plugins:netbox_asset_management:supportcontract_list'
 
 class SupportContractBulkDeleteView(BulkDeleteView):
@@ -214,33 +193,27 @@ class SupportContractChangelogView(ObjectChangeLogView):
 class AssetInformationListView(ObjectListView):
     queryset = AssetInformation.objects.all()
     table = AssetInformationTable
-    template_name = 'netbox_asset_management/asset_list.html'
     filterset = AssetInformationFilterSet
 
 class AssetInformationDetailView(ObjectView):
     queryset = AssetInformation.objects.all()
-    template_name = 'netbox_asset_management/asset_detail.html'
 
 class AssetInformationCreateView(ObjectEditView):
     model = AssetInformation
     form_class = AssetInformationForm
-    template_name = 'netbox_asset_management/asset_edit.html'
 
 class AssetInformationUpdateView(ObjectEditView):
     model = AssetInformation
     form_class = AssetInformationForm
-    template_name = 'netbox_asset_management/asset_edit.html'
 
 class AssetInformationDeleteView(ObjectDeleteView):
     model = AssetInformation
-    template_name = 'netbox_asset_management/asset_delete.html'
 
 class AssetInformationBulkImportView(BulkImportView):
     queryset = AssetInformation.objects.all()
     model = AssetInformation
     table = AssetInformationTable
     form = AssetInformationBulkImportForm
-    template_name = 'netbox_asset_management/asset_import.html'
     default_return_url = 'plugins:netbox_asset_management:asset_list'
 
 class AssetInformationBulkEditView(BulkEditView):
@@ -248,7 +221,6 @@ class AssetInformationBulkEditView(BulkEditView):
     filterset = AssetInformationFilterSet
     table = AssetInformationTable
     form = AssetInformationForm
-    template_name = 'netbox_asset_management/asset_bulk_edit.html'
     default_return_url = 'plugins:netbox_asset_management:asset_list'
 
 class AssetInformationBulkDeleteView(BulkDeleteView):
